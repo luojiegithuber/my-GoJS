1b0b8eb07aeb089f1214ff81cbd4ed6b5ce96db8
研发中心-罗杰 <jie.luo@tcent.cn>
2020-06-23 11:51:02 +0800

    项目环境初始化完成

---
+++ src/components/DemoOne.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="demo1">
+      
+  </div>
+</template>
+
+<script>
+
+import go from 'gojs'
+
+export default {
+  name: 'demo1',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+$error-color:red;
+
+@mixin box-border($border-color: #000) {
+  border: 1px solid $border-color;
+}
+
+@mixin big-box {
+  width: 1000px;
+  height: 800px;
+}
+
+#demo1{
+  @include box-border;
+  @include big-box;
+}
+
+</style>
